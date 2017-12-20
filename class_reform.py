5be3dcbcf5450524bf91798ceeaa9d6f97b31135
Sa'ar <geger40.1@gmail.com>
2017-12-20 14:47:49 -0800

    Update and rename string.py to class_reform.py

--- class_reform.py
+++ class_reform.py
@@ -1,7 +1,9 @@
-class string:
+class refStr:
     def __init__(self, text):
         self.txt = txt
         
     def replace(self, char, withChar):
         txt = char.split(self.txt)
         self.txt = withChar.join(txt)
+    
+    #def prev
