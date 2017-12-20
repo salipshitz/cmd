46accaaca9bcdb43ca20ecc610ce1918641abec4
Sa'ar <geger40.1@gmail.com>
2017-12-20 12:40:13 -0800

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -1,11 +1,66 @@
 from os import system
+from importlib import import_module
+
+meth = {}
+
+def replace(text, char, withChar):
+    text = text.split(char)
+    return withChar.join(text)
+
+def runfunc(nm, args):
+    code = var[nm]["code"]
+    fargs = var[nm]["args"]
+    i = 0
+    codelns = code.splitlines()
+    while i < len(codelns):
+        compiler.compilecode(codelns[i])
+        i += 1
+
+def func(nm, *args):
+    code = ""
+    while True:
+        code += "\n"+input("")
+        if code.endswith("}"):
+            del code[-1]
+            break
+    var[nm] = {"args": args, "code": code}
+
+def echo(*args, sep=' ', end='\n'):
+    text = ""
+    for arg in args:
+        text += arg
+        if arg is not arg[-1]:
+            text += sep
+        else:
+            text += end
+    print(text, end='')
+    return text
+
+def end():
+    exit()
+
+def clear():
+    system('cls')
+
+def get_module(module):
+    mod = import_module(module)
+    meth += mod.meth
 
 var = {"YES": True, "NO": False, "QUOTES": "'\"", "LETTERS_UPPER": "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "LETTERS_LOWER": "abcdefghijklmnopqrstuvwxyz", "LETTERS": "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"}
+meth = {"print": print, "echo": echo, "func": func, "import": get_module}
 
 class compiler:
     @classmethod
     def compileargs(cls, args):
-        args = args.split(" ")
+        args = args.split("**")
+        for i, arg in enumerate(args):
+            if i%2 == 1:
+                args[i] = cls.compilecode(arg)
+        args = " ".join(args)
+        args = args.split("--")
+        for i, arg in enumerate(args):
+            if i%2 == 1:
+                args[i] = var[arg]
         for i, arg in enumerate(args):
             if arg == "is":
                 args[i] = "=="
@@ -41,44 +96,22 @@ class compiler:
         if cmd == "func":
             assert(args[-1] == "{")
             args = args[:-1]
-            print(type(args)
-            args = "".join("".join(args.split("(")).split(")"))
+            print(type(args))
+            args = replace(args, "(", "")
+            args = replace(args, ")", "")
             args[0] = '"{}"'.format(args[0])
         if args != "":
             args = cls.compileargs(args)
-        return exec(cmd+"("+args+")")
-    
+        output = ""
+        try:
+            output = runfunc(cmd, args)
+        except:
+            output = meth[cmd](args)
+
     @classmethod
     def start(cls):
         while True:
             ui = input("# ")
             cls.compilecode(ui)
-
-def replace(text, char, with):
-    
-
-def runfunc(nm, *args):
-    code = var[nm]["code"]
-    fargs = var[nm]["args"]
-    i = 0
-    codelns = code.splitlines()
-    while i < len(codelns):
-        compiler.compilecode(codelns[i])
-        i += 1
-
-def func(nm, *args):
-    code = ""
-    while True:
-        code += "\n"+input("")
-        if code.endswith("}"):
-            del code[-1]
-            break
-    var[nm] = {"args": args, "code": code}
-
-def end():
-    exit()
-
-def clear():
-    system('cls')
 
 compiler.start()
