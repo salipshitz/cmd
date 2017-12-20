3cdb10ccb883d770d6baed189eda7547dbd4af4b
Sa'ar <geger40.1@gmail.com>
2017-12-20 13:40:53 -0800

    Update string.py

--- string.py
+++ string.py
@@ -1,1 +1,7 @@
-
+class string:
+    def __init__(self, text):
+        self.txt = txt
+        
+    def replace(self, char, withChar):
+        txt = char.split(self.txt)
+        self.txt = withChar.join(txt)
